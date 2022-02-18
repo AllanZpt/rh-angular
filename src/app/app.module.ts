80a680753f4fa0ab2be3857a12181ea5eb9f644d
AllanZpt <allanzapaterra@hotmail.com>
2022-02-18 11:48:02 -0300

    Finalização
    Adicionao restante do código, correção de bugs e layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser'; 
-import { HttpClientModule } from '@angular/common/http'
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MuralVagasComponent } from './mural-vagas/mural-vagas.component';
 import { RouterModule } from '@angular/router';
 import { PainelVagasComponent } from './painel-vagas/painel-vagas.component';
-import { RodapeComponent } from './rodape/rodape.component';
 import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
@@ -15,15 +16,15 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     AppComponent,
     MuralVagasComponent,
     PainelVagasComponent,
-    RodapeComponent,
-    MenuComponent
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule,
-    AppRoutingModule
-  
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
